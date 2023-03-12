bf8851089394046796a68376bb521cdf2750aeaa
Dowan <shukang91@gmail.com>
2023-03-12 19:14:16 +0900

    single vr

---
+++ Source/FPSingleVR/FPSingleVRGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSingleVRGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFPSingleVRGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFPSingleVRGameMode();
+};
+
+
+
